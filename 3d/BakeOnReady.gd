0bf52cbb2da3a4ddf65219e490a53172bdd616b4
Pawel Lampe <pawel.lampe@gmail.com>
2022-01-24 18:36:44 +0100

    Add more 3D baking demos

---
+++ 3d/BakeOnReady.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+var _baking_begin_time_ms = null
+
+onready var _navigation_mesh_instance = find_node("NavigationMeshInstance")
+
+
+func _ready():
+	_navigation_mesh_instance.connect("bake_finished", self, "_on_bake_finished")
+	_baking_begin_time_ms = OS.get_ticks_msec()
+	_navigation_mesh_instance.bake_navigation_mesh()
+	print("baking started")
+
+
+func _on_bake_finished():
+	var baking_end_time_ms = OS.get_ticks_msec()
+	print("baking finished, took {0} [ms]".format([baking_end_time_ms - _baking_begin_time_ms]))
