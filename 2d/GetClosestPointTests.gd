02a3c12e975ac81a00d2ff1a5a4158436ad5d5f7
Pawel Lampe <pawel.lampe@gmail.com>
2022-02-11 23:50:16 +0100

    Add 2D tests for `get_closest_point()`

---
+++ 2d/GetClosestPointTests.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	for node in get_children():
+		if not node is Navigation2D:
+			continue
+		var navigation = node
+		for navigation_child in navigation.get_children():
+			if not navigation_child is Position2D:
+				continue
+			var point = navigation_child
+			var closest_point = navigation.get_closest_point(point.position)
+			add_child(_make_path(point.position, closest_point))
+
+
+func _make_path(p1, p2):
+	var line = Line2D.new()
+	line.default_color = Color.black
+	line.width = 2
+	line.add_point(p1)
+	line.add_point(p2)
+	return line
