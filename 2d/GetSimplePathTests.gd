3b6b78f389970bd5ba0f242614a8bc6ea8073d99
Pawel Lampe <pawel.lampe@gmail.com>
2022-02-13 21:17:44 +0100

    Add 2D tests for `get_simple_path()`

---
+++ 2d/GetSimplePathTests.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+export var optimize := false
+
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	for node in get_children():
+		if not node is Navigation2D:
+			continue
+		var navigation = node
+		var begin_point = navigation.find_node("Begin")
+		var end_point = navigation.find_node("End")
+		assert(begin_point and end_point)
+		var path = navigation.get_simple_path(begin_point.position, end_point.position, optimize)
+		print(path)
+		if path == null or path.empty():
+			return
+		add_child(_make_path(path))
+
+
+func _make_path(path):
+	var line = Line2D.new()
+	line.default_color = Color.black
+	line.width = 2
+	for point in path:
+		line.add_point(point)
+	return line
